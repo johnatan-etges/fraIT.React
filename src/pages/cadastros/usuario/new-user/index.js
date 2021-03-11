b072d2ad2bade709e739e6ea63b2f99e6cd08b92
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-03-10 22:10:02 -0300

    User insert working. Next: Add capacibility to adjust input position

--- src/pages/cadastros/usuario/new-user/index.js
+++ src/pages/cadastros/usuario/new-user/index.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import BodyCard from '../../../../components/BodyCard';
 import Header from '../../../../components/header';
 import Footer from '../../../../components/footer';
 import Input from '../../../../components/input';
+
+import api from '../../../../services/api';
 
 // import { Container } from './styles';
 
@@ -12,7 +17,6 @@ function NewUser() {
     const [data, setData] = useState({
         name: "",
         surName: "",
-        login: "",
         login: "",
         email: "",
         passToVerify: "",
@@ -29,7 +33,46 @@ function NewUser() {
             console.log("As senhas não conferem!");
             return
         }
-        setData({password});
+        setData({...data, password});
+    }
+
+    async function createUser() {
+
+        await api.post('/users/signup', {
+            name: data.name,
+            surName: data.surName,
+            login: data.login,
+            email: data.email,
+            password: data.password,
+            avatarURL: data.avatarURL,
+        })
+        .then(response => {
+            toast.success("Usuário criado com sucesso!", {
+                position: toast.POSITION_TOP_RIGHT,
+            });
+        })
+        .catch (err => {                
+            switch (err.response.status) {
+            case 600:
+                toast.error("Nome de usuário e/ou e-mail já cadastrado(s)!", {
+                    position: toast.POSITION_TOP_RIGHT,
+                });
+                break;
+            case 406:
+                toast.error("Os campos 'nome', 'e-mail', 'login' e 'senha' são obrigatórios!", {
+                    position: toast.POSITION_TOP_RIGHT,
+                });
+                break;
+            case 500:
+                toast.error("Não foi possível cadastrar o usuário!", {
+                    position: toast.POSITION_TOP_RIGHT,
+                });
+            default:
+                toast.error("Não foi possível cadastrar o usuário! Erro não identificado!", {
+                    position: toast.POSITION_TOP_RIGHT,
+                });
+            }
+        })
     }
 
   return (
@@ -40,31 +83,31 @@ function NewUser() {
                 id={"user_login"}
                 type={"text"}
                 label={"Nome de usuário"}
-                onBlur={value => setData({login: value})}
+                onBlur={value => setData({...data, login: value})}
             />
             <Input
                 id={"user_name"}
                 type={"text"}
                 label={"Nome"}
-                onBlur={value => setData({name: value})}
+                onBlur={value => setData({...data, name: value})}
             />
             <Input
                 id={"user_surName"}
                 type={"text"}
                 label={"Sobrenome"}
-                onBlur={value => setData({surName: value})}
+                onBlur={value => setData({...data, surName: value})}
             />
             <Input
                 id={"email"}
                 type={"text"}
                 label={"E-mail"}
-                onBlur={value => setData({email: value})}
+                onBlur={value => setData({...data, email: value})}
             />
             <Input
                 id={"senha"}
                 type={"password"}
                 label={"Senha"}
-                onBlur={value => setData({passToVerify: value})}
+                onBlur={value => setData({...data, passToVerify: value})}
             />
             <Input
                 id={"confirmar-senha"}
@@ -73,7 +116,7 @@ function NewUser() {
                 onBlur={value => validaSenha(value)}
             />
 
-            <button>Criar usuário</button>
+            <button onClick={() => createUser()}>Criar usuário</button>
 
         </BodyCard>
         <Footer/>
